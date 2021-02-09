f5a0c851eedece70d35d457664709b7cabbdb3e3
Talha KOCA <mertyigit306@gmail.com>
2021-02-09 16:05:33 +0300

    İlk aşama

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void AddClient(Customer customer)
+        {
+            Console.WriteLine("The customer is added this bank system: " +
+                customer.CustomerName + " " + customer.CustomerLastName);
+        }
+
+        public void RemoveCLient(Customer customer)
+        {
+            Console.WriteLine("This customer account removed from the bank system: " +
+                customer.CustomerName + " " + customer.CustomerLastName);
+        }
+        public void Array(Customer[] customer)
+        {
+            foreach (Customer client in customer)
+            {
+                Console.WriteLine("Customer Name: " + client.CustomerName + 
+                    "\nCustomer Surname: " + client.CustomerLastName + 
+                    "\nPhone Number: " + client.PhoneNumber);
+                Console.WriteLine("------------------------------------");
+
+            }
+           
+        }
+
+    }  
+   
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.CustomerName = "Mesut";
+            customer1.CustomerLastName = "ÖZİL";
+            customer1.PhoneNumber = 0123545688;
+
+            Customer customer2 = new Customer();
+            customer2.CustomerName = "Erol";
+            customer2.CustomerLastName = "BULUT";
+            customer2.PhoneNumber = 0313254979;
+
+            Customer[] customers = new Customer[] { customer1, customer2};
+
+            CustomerManager customerManager = new CustomerManager();
+            Console.WriteLine("---------------Addition-------------");
+            customerManager.AddClient(customer1);
+            customerManager.AddClient(customer2);
+
+            Console.WriteLine("---------------Remove---------------");
+            customerManager.RemoveCLient(customer1);
+            customerManager.RemoveCLient(customer2);
+
+            Console.WriteLine("---------------Array----------------");
+            customerManager.Array(customers);
+        }
+    }
+}
